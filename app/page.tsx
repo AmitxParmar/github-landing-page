ceda608bc6f0b5d5bd7d05063698c502629941c9
AmitxParmar <amitparmar901@gmail.com>
2025-02-23 23:33:29 +0530

    complete: Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import Navbar from "./components/Navbar/Navbar";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="relative z-50">
+        <div className="absolute">
+          <Navbar />
+        </div>
+      </div>
+    </div>
+  );
 }
